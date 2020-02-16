c53003b888d219d18f744a56230f8ed4308c0086
twister2k33 <twister2k33@gmail.com>
2020-02-15 18:24:52 -0600

    test

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium_Practice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium_Practice")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5cba41b-d1ed-4c20-a77d-2eff0c45cdb7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace Selenium_Practice
+{
+
+
+    public class EntryPoint
+    {
+
+        public IWebDriver driver;
+
+        [SetUp]
+
+        public void Initialize()
+        {
+
+            driver = new ChromeDriver();
+
+
+        }
+
+
+
+
+        [Test]
+
+        public void Test1()
+        {
+
+            driver.Navigate().GoToUrl("http://duckduckgo.com");
+
+            Thread.Sleep(3000);
+
+            IWebElement textSearch = driver.FindElement(By.Id("search_form_input_homepage"));
+            IWebElement submit = driver.FindElement(By.Id("search_button_homepage"));
+
+            textSearch.SendKeys("Seinfeld of course");
+            submit.Click();
+
+            
+
+
+
+        }
+
+
+
+
+
+        [TearDown]
+
+        public void EndTest()
+        {
+
+            driver.Quit();
+        }
+
+
+
+
+
+    }
+
+
+
+    }
+
